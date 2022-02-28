c851764d3562f7259ff101d74abde8140e48d0bc
Olzhik <180103273@stu.sdu.edu.kz>
2022-02-28 21:59:38 +0600

    Set up the project and add dependencies

---
+++ src/main/java/com/example/diplomaprojectgeneticcode/DiplomaProjectGeneticCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplomaprojectgeneticcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectGeneticCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaProjectGeneticCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/diplomaprojectgeneticcode/DiplomaProjectGeneticCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diplomaprojectgeneticcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaProjectGeneticCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
